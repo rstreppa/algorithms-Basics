07d40a89824d5b241921c3863ed0e5a18cb28600
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-07-29 17:33:01 -0400

    26. Remove Duplicates from Sorted Array
    26. Remove Duplicates from Sorted Array
    easy, did it on the spot

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -212,6 +212,37 @@ def productExceptSelf2(nums):
         
     return res
 
+
+def removeDuplicates(nums):
+    """
+    26. Remove Duplicates from Sorted Array
+    Easy
+    Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+    The relative order of the elements should be kept the same.
+    Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. 
+    More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. 
+    It does not matter what you leave beyond the first k elements.
+    Return k after placing the final result in the first k slots of nums.
+    Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.   
+    
+    :type nums: List[int]
+    :rtype: int
+    """
+    n   = len(nums)
+    if n == 0:
+        return nums
+    i   = 1
+    while i < n:
+        if nums[i] == nums[i-1]:
+            nums.pop(i)
+            n   -= 1
+        else:
+            i   += 1
+    return n
+
+
+
+
 def findDuplicate(nums):
     ''' Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
         There is only one repeated number in nums, return this repeated number.
