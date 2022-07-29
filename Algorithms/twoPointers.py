5cd0b7e58499160e43bc74df85b7c1005d7c7a21
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-07-29 15:46:53 -0400

    18. 4Sum
    18. 4Sum
    easy variation on two pointer techinque, now you know how to do it

--- Algorithms/twoPointers.py
+++ Algorithms/twoPointers.py
@@ -185,7 +185,45 @@ def threeSumClosest(nums, target):
                 res = val
     return res        
         
-        
+
+def fourSum(nums, target):
+    """
+    18. 4Sum
+    Medium
+    Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+    0 <= a, b, c, d < n
+    a, b, c, and d are distinct.
+    nums[a] + nums[b] + nums[c] + nums[d] == target
+    You may return the answer in any order.
+    
+    Easy two pointer technique
+    Notice that you remove duplicate solutions by returning a set
+    
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[List[int]]
+    """
+    # two pointer I would say
+    nums    = sorted(nums)
+    n       = len(nums)
+    res     = set()
+    for i in range( 0, n-2 ):
+        for j in range( i+1, n-1 ):
+            l   = j+1
+            r   = n-1
+            while l < r:
+                if nums[i] + nums[j] + nums[l] + nums[r] == target:
+                    res.add( ( nums[i], nums[j], nums[l], nums[r] ) )
+                    l += 1
+                    r -= 1
+                elif nums[i] + nums[j] + nums[l] + nums[r] < target:
+                    l += 1
+                else:
+                    r -= 1
+    return res
+    
+    
+    
 def removeDuplicates( nums ):
     ''' Remove Duplicates from Sorted Array  in-place such that each unique element appears only once. 
         The relative order of the elements should be kept the same. 
