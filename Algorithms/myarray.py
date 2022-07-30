b1ef7c2668ed233ddbd3667e0e64abbd58e2c688
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-07-30 13:30:39 -0400

    31. Next Permutation
    31. Next Permutation
    interesting problem to solve, easy once you get the idea

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -980,4 +980,64 @@ def numberOfArithmeticSlices(nums):
         else:
             count_sum = 1
             
-    return ans            
+    return ans   
+
+def nextPermutation(self, nums):
+    """
+        31. Next Permutation
+        Medium
+        A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+        For example, for arr = [1,2,3], the following are considered permutations of arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
+        The next permutation of an array of integers is the next lexicographically greater permutation of its integer. 
+        More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, 
+        then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, 
+        the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+        For example, the next permutation of arr = [1,2,3] is [1,3,2].
+        Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+        While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+        Given an array of integers nums, find the next permutation of nums.
+
+        The replacement must be in place and use only constant extra memory.
+        nums = [4,5,3,2,1]
+        Step 1: scan from right to left and stop at 4 because it less than 5. Here, index = 0
+        Step 2: Again scan from right to left and stop at 5 because it is greater than 4. Here, j = 1
+        Step 3: Swap the elements at index and j. The array will become [5,4,3,2,1].
+        Step 4: Reverse the array after index. The array will become [5,1,2,3,4]
+        
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+    """    
+    def reverse(nums, i, j):
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+
+
+    # Length of the array
+    n = len(nums)
+    # Index of the first element that is smaller than
+    # the element to its right.
+    index = -1
+    # Loop from right to left
+    for i in range(n - 1, 0, -1):
+        if nums[i] > nums[i - 1]:
+            index = i - 1
+            break
+    # Base condition
+    if index == -1:
+        reverse(nums, 0, n - 1)
+        return
+    j = n - 1
+    # Again swap from right to left to find first element
+    # that is greater than the above find element
+    for i in range(n - 1, index, -1):
+        if nums[i] > nums[index]:
+            j = i
+            break
+    # Swap the elements
+    nums[index], nums[j] = nums[j], nums[index]
+    # Reverse the elements from index + 1 to the nums.length
+    reverse(nums, index + 1, n - 1)
