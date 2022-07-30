f61d4eb0cab7fc7fda00fd238b2d79a23be3c465
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-07-30 15:37:04 -0400

    39. Combination Sum
    39. Combination Sum
    added title

--- Algorithms/backtracking.py
+++ Algorithms/backtracking.py
@@ -182,7 +182,10 @@ def combine(n, k):
     return res
 
 def combinationSum(candidates, target):
-    ''' Given an array of distinct integers candidates and a target integer target, 
+    ''' 
+    	39. Combination Sum
+	Medium
+    	Given an array of distinct integers candidates and a target integer target, 
         return a list of all unique combinations of candidates where the chosen numbers sum to target. 
         You may return the combinations in any order.
         The same number may be chosen from candidates an unlimited number of times. 
