a6d8ae570ae18b0f95d1545ff22a70e9b7618733
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-07-31 11:13:57 -0400

    45. Jump Game II
    45. Jump Game II
    study greedy algorithm by looking at the two can jump problems

--- Algorithms/greedy.py
+++ Algorithms/greedy.py
@@ -122,3 +122,34 @@ def canJump(nums):
         if i + nums[i] >= target:
             target = i
     return target == 0    
+
+def jump(self, nums):
+    """
+        45. Jump Game II
+        Medium
+        Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+        Each element in the array represents your maximum jump length at that position.
+        Your goal is to reach the last index in the minimum number of jumps.
+        You can assume that you can always reach the last index.
+            
+        1-dim BFS Breadth First Search
+        count the intervals [left, right] which propagate from the initial value until you reach the end
+        the new left  = right + 1
+        the new right = max distance from previous points in previous interval
+
+        :type nums: List[int]
+        :rtype: int
+    """
+    
+    res     = 0
+    l = r   = 0
+    
+    while r < len(nums) - 1:
+        farthest    = 0
+        for i in range( l, r+1 ):
+            farthest = max( farthest, i+nums[i] )
+        l   = r + 1
+        r   = farthest
+        res += 1
+    
+    return res
