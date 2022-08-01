037a327832741a8d19d0434f50e8827edf59c925
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-01 16:07:17 -0400

    173. Binary Search Tree Iterator
    173. Binary Search Tree Iterator
    study this as it's going to be useful in other problems

--- DataStructures/binarySearchTree.py
+++ DataStructures/binarySearchTree.py
@@ -441,3 +441,59 @@ class Solution(object):
         root.right  = self.sortedListToBST( slow.next )
         
         return root
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator(object):
+    """
+        173. Binary Search Tree Iterator
+        Medium
+        Implement the BSTIterator class that represents an iterator over the in-order traversal 
+        of a binary search tree (BST):
+        BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. 
+        The root of the BST is given as part of the constructor. 
+        The pointer should be initialized to a non-existent number smaller than any element in the BST.
+        boolean hasNext() Returns true if there exists a number in the traversal 
+        to the right of the pointer, otherwise returns false.
+        int next() Moves the pointer to the right, then returns the number at the pointer.
+        Notice that by initializing the pointer to a non-existent smallest number, 
+        the first call to next() will return the smallest element in the BST.
+        You may assume that next() calls will always be valid. That is, there will be at least a next
+        number in the in-order traversal when next() is called.    
+    """
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack  = []
+        while root:
+            self.stack.append( root )
+            root    = root.left
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        res         = self.stack.pop()
+        curr        = res.right
+        while curr:
+            self.stack.append( curr )
+            curr    = curr.left
+        return res.val
+        
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self.stack != []
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
