1c7e0ebf6eddd891504916cca9df1bf170f6c2f5
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-01 22:03:31 -0400

    255. Verify Preorder Sequence in Binary Search Tree (Python)
    255. Verify Preorder Sequence in Binary Search Tree (Python)
    need to review this as it is not clear

--- DataStructures/binarySearchTree.py
+++ DataStructures/binarySearchTree.py
@@ -565,3 +565,32 @@ class Solution(object):
         
         if root.right:
             self.dfs( root.right, k )
+
+
+def verifyPreorder(preorder):
+    '''
+    	255. Verify Preorder Sequence in Binary Search Tree (Python)
+	Medium
+	Stack .
+	Description
+	Given an array of numbers, verify whether it is the correct preorder traversal sequence of a binary search tree.
+	You may assume each number in the sequence is unique.
+		
+	Methodology
+	Kinda simulate the traversal, keeping a stack of nodes (just their values) of which we’re still in the left subtree. 
+	If the next number is smaller than the last stack value, then we’re still in the left subtree of all stack nodes, 
+	so just push the new one onto the stack. But before that, pop all smaller ancestor values, as we must now be in their right subtrees 
+	(or even further, in the right subtree of an ancestor). Also, use the popped values as a lower bound, since being in their right subtree 
+	means we must never come across a smaller number anymore.
+    '''
+    if not preorder: 
+	return True
+    min_val 	= float('-inf')
+    s 		= []
+    for elem in preorder:
+        if elem < min_val: 
+	    return False
+        while s and s[-1] < elem:
+            min_val = s.pop()
+        s.append(elem)
+    return True
