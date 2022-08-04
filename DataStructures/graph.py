8902e020cd8dd06aa55b3a28630666a9309b50fe
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-03 23:08:30 -0400

    547. Number of Provinces
    547. Number of Provinces
    easy to do once you convert to default dict and apply a BFS with a queue

--- DataStructures/graph.py
+++ DataStructures/graph.py
@@ -587,3 +587,45 @@ def canVisitAllRooms(self, rooms):
         return True
     else:
         return False
+    
+def findCircleNum(isConnected):
+    """
+        547. Number of Provinces
+        Medium
+        There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, 
+        and city b is connected directly with city c, then city a is connected indirectly with city c.
+        A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+        You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, 
+        and isConnected[i][j] = 0 otherwise.
+        Return the total number of provinces.
+        :type isConnected: List[List[int]]
+        :rtype: int
+    """
+    
+    n       = len(isConnected)
+    d       = defaultdict(list)
+    
+    for i in range(n):
+        for j in range(n):
+            if isConnected[i][j]:
+                d[i].append(j)
+                d[j].append(i)
+                
+    res     = 0
+    visited = set()
+    q       = []
+    for i in range(n):
+        if i in visited:
+            continue
+        else:
+            visited.add(i)
+            res     += 1
+            q.append(i)
+            while q:
+                v   = q.pop(0)
+                visited.add(v)
+                for neighbor in d[v]:
+                    if neighbor not in visited:
+                        q.append(neighbor)
+    return res
+
