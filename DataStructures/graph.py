c8daa1afd2d03146ccf5a951629ba2c56574f539
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-03 22:36:13 -0400

    841. Keys and Rooms
    841. Keys and Rooms
    easy to solve with a BFS queue

--- DataStructures/graph.py
+++ DataStructures/graph.py
@@ -557,3 +557,33 @@ def findSmallestSetOfVertices(n, edges):
             res.append(i)
     return res
 
+def canVisitAllRooms(self, rooms):
+    """
+        841. Keys and Rooms
+        Medium
+        There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. 
+        However, you cannot enter a locked room without having its key.
+
+        When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, 
+        and you can take all of them with you to unlock the other rooms.
+
+        Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, 
+        or false otherwise.        
+        :type rooms: List[List[int]]
+        :rtype: bool
+    """
+    n           = len(rooms)
+    visited     = set()
+    q           = [0]
+    
+    while q:
+        v   = q.pop(0)
+        visited.add(v)
+        for e in rooms[v]:
+            if e not in visited:
+                q.append(e)
+    
+    if visited == set( range(n) ):
+        return True
+    else:
+        return False
