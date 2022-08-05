8e5b1529ed96835d98e9043edd96abcc1f2a490f
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-05 16:54:11 -0400

    88. Merge Sorted Array
    88. Merge Sorted Array
    start from the end to have O(m+n) and O(1) space

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -1047,3 +1047,35 @@ def nextPermutation(self, nums):
     nums[index], nums[j] = nums[j], nums[index]
     # Reverse the elements from index + 1 to the nums.length
     reverse(nums, index + 1, n - 1)
+
+def merge(nums1, m, nums2, n):
+    """
+        88. Merge Sorted Array
+        Easy
+        You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+        and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+        Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+        The final sorted array should not be returned by the function, but instead be stored 
+        inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m
+        elements denote the elements that should be merged, and the last n elements are set to 0 
+        and should be ignored. nums2 has a length of n.        
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+    """
+    while m > 0 and n > 0:
+		if nums1[m - 1] >= nums2[n - 1]:
+			nums1[m + n - 1] = nums1[m - 1]
+			m -= 1
+		else:
+			nums1[m + n - 1] = nums2[n - 1]
+			n -= 1
+
+    for i in range(0, n):
+		nums1[i] = nums2[i]
+
+    return nums1
