d4c746eb4bbd9521bca00d01e7fb1e05af9e5d0b
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-06 16:56:16 -0400

    598. Range Addition II
    598. Range Addition II
    easy once you understand the trick of taking the min

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -1328,3 +1328,28 @@ def findMaxConsecutiveOnes(nums):
                 res = max(res, d[key] + d[key + 1])
 
         return res        
+
+    def maxCount(self, m, n, ops):
+        """
+            598. Range Addition II
+            Easy
+            You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] 
+            should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
+            Count and return the number of maximum integers in the matrix after performing all the operations.
+
+            So , solution is to simple find out the product of minimum of first co-ordinates of cell and minimum of second co-ordinates of cell.
+            We are taking product because we need to find out the count of the maximum integer in the matrix after performing all the given operations.
+
+            :type m: int
+            :type n: int
+            :type ops: List[List[int]]
+            :rtype: int
+        """
+        length = len(ops)
+        if length == 0:
+            return m*n
+        result = [ops[0][0] , ops[0][1]]
+        for i in range(1,length):
+            result[0] = min(result[0] , ops[i][0])
+            result[1] = min(result[1] , ops[i][1])
+        return result[0]*result[1]                    
