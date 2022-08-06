c9ed916ee3c038b8b9943a88d740577cf142435f
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-06 12:44:16 -0400

    506. Relative Ranks
    506. Relative Ranks
    easy you need to keep in mind to always have a dict in these sort of problems

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -1272,3 +1272,37 @@ def findMaxConsecutiveOnes(nums):
                     res[s.pop()]    = num
                 s.append(i)
         return res
+
+    def findRelativeRanks(self, score):
+        """
+        506. Relative Ranks
+        Easy
+        You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+        The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on.
+        The placement of each athlete determines their rank:
+
+        The 1st place athlete's rank is "Gold Medal".
+        The 2nd place athlete's rank is "Silver Medal".
+        The 3rd place athlete's rank is "Bronze Medal".
+        For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+        Return an array answer of size n where answer[i] is the rank of the ith athlete.
+        :type score: List[int]
+        :rtype: List[str]
+        """
+        
+        sortedNums  = sorted(score, reverse=True)
+        d           = {}
+        for i in range(len(sortedNums)):
+            if i == 0:
+                d[sortedNums[i]] = 'Gold Medal'
+            elif i == 1:
+                d[sortedNums[i]] = 'Silver Medal'
+            elif i == 2:
+                d[sortedNums[i]] = 'Bronze Medal'
+            else:
+                d[sortedNums[i]] = str(i+1)
+        res     = []
+        for num in score:
+            res.append(d[num])
+        return res
