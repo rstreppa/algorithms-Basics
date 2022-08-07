2ca1ce9ce0eb9991ec007a80b7c94c913e6bcf0b
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-07 13:49:43 -0400

    744. Find Smallest Letter Greater Than Target
    744. Find Smallest Letter Greater Than Target
    sorted letters is a giveaway for binary search
    letters wrap around so take the mod

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -1564,4 +1564,25 @@ def helper(self, image, i, j, old_color, new_color):
         self.helper(image, i, j - 1, old_color, new_color)            
         self.helper(image, i, j + 1, old_color, new_color)            
         self.helper(image, i - 1, j, old_color, new_color)            
-        self.helper(image, i + 1, j, old_color, new_color)        
+        self.helper(image, i + 1, j, old_color, new_color)      
+	
+def nextGreatestLetter(self, letters, target):
+    """
+    744. Find Smallest Letter Greater Than Target
+    Easy
+    Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array 
+    that is larger than target.
+    Note that the letters wrap around.
+    :type letters: List[str]
+    :type target: str
+    :rtype: str
+    """
+    l           = 0
+    r           = len(letters) - 1
+    while l <= r:
+        mid     = (l + r)//2
+        if letters[mid] > target:
+            r   = mid -1
+        else:
+            l   = mid + 1
+    return letters[l % len(letters)]
