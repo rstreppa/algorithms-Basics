d7765e95a9fd28afc1934f9aee29fdc805609a42
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-07 13:38:53 -0400

    733. Flood Fill
    733. Flood Fill
    easy once you know how to set the recursive call

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -1520,3 +1520,48 @@ def isOneBitCharacter(self, bits):
         else:
             i += 2
     return False
+
+def floodFill(self, image, sr, sc, newColor):
+    """
+    733. Flood Fill
+    Easy
+    An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+    You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+    To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel,
+    plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. 
+    Replace the color of all of the aforementioned pixels with color.
+
+    Recursive solution O(m*n)
+
+    Return the modified image after performing the flood fill.
+    :type image: List[List[int]]
+    :type sr: int
+    :type sc: int
+    :type color: int
+    :rtype: List[List[int]]
+    """
+    
+    old_color = image[sr][sc]
+    
+    if old_color != newColor:
+        self.helper(image, sr, sc, old_color, newColor)
+    
+    return image
+    
+def helper(self, image, i, j, old_color, new_color):
+    
+    if i < 0 or i > len(image) - 1:
+        return
+    
+    if j < 0 or j > len(image[0]) - 1:
+        return
+    
+    if image[i][j] != old_color:
+        return
+    else:
+        image[i][j] = new_color
+        
+        self.helper(image, i, j - 1, old_color, new_color)            
+        self.helper(image, i, j + 1, old_color, new_color)            
+        self.helper(image, i - 1, j, old_color, new_color)            
+        self.helper(image, i + 1, j, old_color, new_color)        
