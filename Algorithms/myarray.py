548f8055df9f4091f1f9ad25265388499b774eb2
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-06 21:02:48 -0400

    661. Image Smoother
    661. Image Smoother
    easy if you consider dx and dy

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -1394,3 +1394,28 @@ def findMaxConsecutiveOnes(nums):
                 res.append(common[i][0])
         
         return res
+
+def imageSmoother(self, M):
+    """
+        661. Image Smoother
+        Easy
+        An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down the average of the cell and the eight
+        surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, 
+        we do not consider it in the average (i.e., the average of the four cells in the red smoother).
+        :type img: List[List[int]]
+        :rtype: List[List[int]]
+    """
+    m       = len(M)
+    n       = len(M[0])
+    res     = [[0]*n for i in range(m)]
+    for i in range(m):
+        for j in range(n):
+            t = 0
+            c = 0
+            for dx in range(-1,2):
+                for dy in range(-1,2):
+                    if 0<=i+dx<m and 0<=j+dy<n:
+                        t += M[i+dx][j+dy] 
+                        c += 1
+            res[i][j] = t//c
+    return res
