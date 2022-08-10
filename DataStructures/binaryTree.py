3ee451be55c35a8612f341be892dd9f302b51ea0
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-10 14:38:49 -0400

    124. Binary Tree Maximum Path Sum
    124. Binary Tree Maximum Path Sum
    really hard problem, watch video first

--- DataStructures/binaryTree.py
+++ DataStructures/binaryTree.py
@@ -488,7 +488,44 @@ def isSymmetric(root):
     
     return isMirror( root, root )	
 
-	
+def maxPathSum(self, root):
+    """
+    124. Binary Tree Maximum Path Sum
+    Hard
+    A path in a binary tree is a sequence of nodes where each pair of adjacent nodes 
+    in the sequence has an edge connecting them. 
+    A node can only appear in the sequence at most once. 
+    Note that the path does not need to pass through the root.
+    The path sum of a path is the sum of the node's values in the path.
+
+    DFS solution with a twist 
+    watch neetcode https://www.youtube.com/watch?v=Hr5cWUld4vU
+    O(n) O(n)
+        
+    Given the root of a binary tree, return the maximum path sum of any non-empty path.
+    :type root: TreeNode
+    :rtype: int
+    """
+        
+    res             = [ root.val ]      # global variable
+        
+    # return max path sum without split
+    def dfs( root ):
+        if not root:
+            return 0
+            
+        leftMax     = dfs( root.left )
+        rightMax    = dfs( root.right )
+        leftMax     = max( leftMax, 0 )
+        rightMax    = max( rightMax, 0 )
+            
+        # compute max path sum WITH split 
+        res[0]      = max( res[0], root.val + leftMax + rightMax )
+            
+        return root.val + max( leftMax, rightMax )
+        
+    dfs(root)
+    return res[0]
 	
 def hasPathSum(root, targetSum):
     ''' Given the root of a binary tree and an integer targetSum, return true 
