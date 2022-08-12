36454c95727f1604e5f9039a22c37dc2508f5b9c
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-11 22:36:26 -0400

    997. Find the Town Judge
    997. Find the Town Judge
    better implementation

--- DataStructures/myhash.py
+++ DataStructures/myhash.py
@@ -426,24 +426,15 @@ def findJudge(self, n, trust):
         :type trust: List[List[int]]
         :rtype: int
     """
-    arr     = []
-    d       = {}
-        
-    for i in range(1, n+1):
-        arr.append(i)
-        d[i] = []
-            
-    for i in trust:
-        d[i[1]].append(i[0])
-        if i[0] in arr:
-            arr.remove(i[0])
-        
-    if len(arr)!=1:
-        return -1
-        
-    for key, value in d.items():
-        if key==arr[0] and len(value)==n-1:
-            return key
-        
-    return -1        
+    d       = defaultdict(set)
+    dnot    = defaultdict(set)
+    for edge in trust:
+        u, v    = edge
+        d[v].add(u)
+        dnot[u].add(v)
+     
+    for i in range( 1, n+1 ):
+        if len( d[i] ) == n-1 and len( dnot[i] ) == 0:
+            return i
+    return -1
 
