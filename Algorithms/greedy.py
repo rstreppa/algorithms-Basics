3c5919b427f506a0bae71b76427fe01de70800d0
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-12 16:32:34 -0400

    1005. Maximize Sum Of Array After K Negations
    1005. Maximize Sum Of Array After K Negations
    added title to the problem

--- Algorithms/greedy.py
+++ Algorithms/greedy.py
@@ -8,7 +8,9 @@
 """
 
 def largestSumAfterKNegations(A, K):
-    ''' Given an integer array nums and an integer k, modify the array in the following way:
+    ''' 
+        1005. Maximize Sum Of Array After K Negations
+        Given an integer array nums and an integer k, modify the array in the following way:
         choose an index i and replace nums[i] with -nums[i].
         You should apply this process exactly k times. You may choose the same index i multiple times.
         Return the largest possible sum of the array after modifying it in this way.
