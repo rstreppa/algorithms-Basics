76380a769e2f8f4cb56aaa4c6ec5ca559ddb1333
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-12 20:01:02 -0400

    1150. Check If a Number Is Majority Element in a Sorted Array
    1150. Check If a Number Is Majority Element in a Sorted Array
    study this example to see how to find the first occurrence of a repeated target in a sorted array

--- Algorithms/binarysearch.py
+++ Algorithms/binarysearch.py
@@ -306,28 +306,28 @@ def searchRange(self, nums, target):
                 left = middle + 1
         return firstOccurrence
 
-
-    def findLastOccurrence(nums, target):
-        # Left snd right pointers
-        left, right = 0, len(nums) - 1
-        # Index of first occurrence
-        lastOccurrence = -1
-        # Loop until the two pointers meet
-        while left <= right:
-            # Middle index
-            middle = left + (right - left) // 2
-            # Check if we have found the value
-            if target == nums[middle]:
-                lastOccurrence = middle
-                left = middle + 1
-            # If the target is less than the element
-            # at the middle index
-            elif target < nums[middle]:
-                right = middle - 1
-            # If the target is greater than the element
-            # at the middle index
-            else:
-                left = middle + 1
-        return lastOccurrence
-
-    return [findFirstOccurrence(nums, target), findLastOccurrence(nums, target)]
+def isMajorityElement( nums, target ):
+    """ 
+        1150. Check If a Number Is Majority Element in a Sorted Array
+        Given an array nums sorted in non-decreasing order, and a number target, return True if and only if target is a majority element.
+        A majority element is an element that appears more than N/2 times in an array of length N.
+        
+        Since the array is sorted, use binary search.
+        Find the first occurrence of target, you do so by requiring if nums[mid] >= target: r = mid
+        which will shrink the seacrh interval to the left
+        Finally once found the first occurence of target, check that l + len(nums)//2 is still equal to target and is within bounds
+    """
+    
+    if not nums:
+        return False
+          
+    n           = len(nums)
+    l           = 0
+    r           = n - 1
+    while l < r:
+        mid     = l + (r - l) // 2
+        if nums[mid] < target:
+            l = mid + 1
+        else:
+            r = mid
+    return l + n // 2 < n and nums[l + n // 2] == target
