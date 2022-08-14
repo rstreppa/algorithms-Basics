929ebc2db66e2f7b648cf8c164c900e40831b11b
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-14 18:01:19 -0400

    1176: Diet Plan Performance
    1176: Diet Plan Performance
    easy sliding window technique problem

--- Algorithms/slidingwindow.py
+++ Algorithms/slidingwindow.py
@@ -89,6 +89,52 @@ def lengthOfLongestSubstring(s):
             i += 1
     return res
 
+def dietPlanPerformance( calories, k, lower, upper ):
+    """
+        1176: Diet Plan Performance
+        Easy
+        A dieter consumes calories[i] calories on the i-th day. 
+        Given an integer k, for every consecutive sequence of k days (calories[i], calories[i+1], ..., calories[i+k-1] for all 0 <= i <= n-k), 
+        they look at T, the total calories consumed during that sequence of k days (calories[i] + calories[i+1] + ... + calories[i+k-1]):
 
+        If T < lower, they performed poorly on their diet and lose 1 point; 
+        If T > upper, they performed well on their diet and gain 1 point;
+        Otherwise, they performed normally and there is no change in points.
+        Initially, the dieter has zero points. Return the total number of points the dieter has after dieting for calories.length days.
+        Note that the total points can be negative.
+  
+        Example 3:
+        Input: calories = [6,5,0,0], k = 2, lower = 1, upper = 5
+        Output: 0
+        Explanation:
+        calories[0] + calories[1] > upper so 1 point is gained.
+        lower <= calories[1] + calories[2] <= upper so no change in points.
+        calories[2] + calories[3] < lower so 1 point is lost.  
+  
+        Sliding window Technique
+        
+    """
+    if not calories:
+        return 0
+    
+    rollingSum          = 0
+    res                 = 0
+    for i in range( len(calories) ):
+        rollingSum      += calories[i]
+        if i < k - 1:
+            continue
+        # when to pop out element on the left
+        if i >= k:
+            rollingSum -= calories[i - k];
+        if rollingSum > upper:
+            res         += 1
+        if rollingSum < lower:
+            res         -= 1
+    return res
+        
+        
+        
+        
+    
 
 
