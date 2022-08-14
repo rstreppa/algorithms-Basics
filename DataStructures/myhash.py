19aae24732d1da2efe72b690c63285cdff513835
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-14 16:45:03 -0400

    1331. Rank Transform of an Array
    1331. Rank Transform of an Array
    easy: use a hashmap to map values to original array

--- DataStructures/myhash.py
+++ DataStructures/myhash.py
@@ -486,3 +486,23 @@ def countCharacters(self, words, chars):
     
 
     return res        
+
+def arrayRankTransform(self, arr):
+    """
+        1331. Rank Transform of an Array
+        Easy
+        Given an array of integers arr, replace each element with its rank.
+        The rank represents how large the element is. The rank has the following rules:
+        Rank is an integer starting from 1.
+        The larger the element, the larger the rank. If two elements are equal, their rank must be the same.
+        Rank should be as small as possible.
+        
+        Python Elegant & Short | Two lines | HashMap + Sorting
+        Time:   O(n*log(n))
+        Memory: O(n)
+        
+        :type arr: List[int]
+        :rtype: List[int]
+    """
+    ranks = {num: r for r, num in enumerate(sorted(set(arr)), start=1)}
+    return [ranks[num] for num in arr]
