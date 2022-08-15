d5b3c0d7ea96e4fbe3ee48f8216208f57c4d7a3f
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2022-08-15 15:06:16 -0400

    Circular Queue
    Circular Queue
    take a look as it may appear in a question

--- DataStructures/queue.py
+++ DataStructures/queue.py
@@ -91,4 +91,54 @@ def reverse_queue_rec(queue):
     reverse_queue_rec(queue)
  
     # Enqueue current item (to rear)
-    queue.put(item)    
+    queue.put(item)  
+    
+# This is the CircularQueue class
+class CircularQueue:
+    """
+    A Circular Queue is a queue data structure but circular in shape, therefore after the last position, the next place in the queue is the first position.
+    https://www.studytonight.com/code/python/ds/circular-queue-in-python.php
+    """
+  
+  # constructor for the class
+  # taking input for the size of the Circular queue 
+  # from user
+  def __init__(self, maxSize):
+    self.queue = list()
+    # user input value for maxSize
+    self.maxSize = maxSize
+    self.head = 0
+    self.tail = 0
+
+  # add element to the queue
+  def enqueue(self, data):
+    # if queue is full
+    if self.size() == (self.maxSize - 1):
+      return("Queue is full!")
+    else:
+      # add element to the queue
+      self.queue.append(data)
+      # increment the tail pointer
+      self.tail = (self.tail+1) % self.maxSize
+      return True
+  
+  # remove element from the queue
+  def dequeue(self):
+    # if queue is empty
+    if self.size() == 0:
+      return("Queue is empty!")
+    else:
+      # fetch data
+      data = self.queue[self.head]
+      # increment head
+      self.head = (self.head+1) % self.maxSize
+      return data
+  
+  # find the size of the queue
+  def size(self):
+    if self.tail >= self.head:
+      qSize = self.tail - self.head
+    else:
+      qSize = self.maxSize - (self.head - self.tail)
+    # return the size of the queue
+    return qSize
