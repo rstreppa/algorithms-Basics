1a89863af3bea8ff0f81ee3097e601eb7b70fae1
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2023-09-17 09:55:33 -0500

    removed 200. Number of Islands, now in LeetCode

--- Algorithms/myarray.py
+++ Algorithms/myarray.py
@@ -1778,50 +1778,6 @@ def diStringMatch(self, s):
 
     return ans
 
-def numIslands(self, grid):
-    """
-    200. Number of Islands
-    Medium
-    Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
-    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
-    You may assume all four edges of the grid are all surrounded by water
-    
-    Use ideas from graphs
-    BFS solution to identify islands (neighbor layers)
-    
-    watch this video
-    https://www.google.com/search?q=200.+Number+of+Islands+leetcode+solution+python&rlz=1C1CHBF_enUS894US894&oq=200.+Number+of+Islands+leetcode+solution++python&aqs=chrome..69i57.10689j0j7&sourceid=chrome&ie=UTF-8#kpvalbx=_daTyYorGBtyj5NoPleeHkAE25
-    
-    :type grid: List[List[str]]
-    :rtype: int
-    """
-    
-    if not grid:
-        return 0
-    
-    rows, cols  = len(grid), len(grid[0])
-    visited     = set()
-    res         = 0
-    
-    def bfs(r, c):
-        q       = []
-        visited.add( (r,c) )
-        q.append( (r,c) )
-        while q:
-            row, col    = q.pop(0)
-            directions  = [ [1,0], [-1,0], [0,1], [0,-1] ]
-            for dr, dc in directions:
-                r, c    = row + dr, col + dc
-                if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visited:
-                    q.append( (r,c) )
-                    visited.add( (r,c) )
-    
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == "1" and (r,c) not in visited:
-                bfs(r,c)
-                res += 1
-    return res
 
 
 def pacificAtlantic(self, heights):
