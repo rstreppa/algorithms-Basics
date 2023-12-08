90055dafad1c4d111c6452351b0863d5f17f3f7e
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2023-12-08 14:06:27 -0600

    migrated a couple of problems to Leetcode repo

--- DataStructures/myhash.py
+++ DataStructures/myhash.py
@@ -10,40 +10,6 @@
 import math
 from collections import Counter
 
-
-def largestUniqueNumber(A):
-    ''' Given an array of integers A, return the largest integer that only occurs once.
-        If no integer occurs once, return -1. 
-    '''
-    d = {}
-    for elem in A:
-        d[elem] = d.get(elem, 0) + 1
-    arr = []
-    for k, v in d.items():
-        if v == 1:
-            arr.append(k)
-    return -1 if not arr else sorted(arr)[-1]
-
-def countCharacters(words, chars):
-    ''' You are given an array of strings words and a string chars.
-        A string is good if it can be formed by characters from chars (each character can only be used once).
-        Return the sum of lengths of all good strings in words.
-    ''' 
-    d = {}
-    for c in chars:
-        d[c] = d.get(c, 0) + 1
- 
-    def check(word, chars, d):
-        if len(word) > len(chars):
-            return False
-        temp = {}
-        for c in word:
-            temp[c] = temp.get(c, 0) + 1
-            if c not in d.keys() or d[c] < temp[c]:
-                return False
-        return True
-
-    return sum(len(word) for word in words if check(word, chars, d))       
         
 def calculateTime(keyboard, word):
     ''' There is a special keyboard with all keys in a single row.
